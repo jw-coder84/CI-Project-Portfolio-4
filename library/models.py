ca4630e79d823ea187249910fd473cce2979523f
jw-coder84 <jmw_19@hotmail.com>
2022-11-01 15:04:26 +0000

    Added requirement and added db models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+import uuid
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    synopsis = models.TextField(max_length=1000, help_text='Enter a brief \
+        description of the book')
+    isbn = models.PositiveBigIntegerField(max_length=13, unique=True)
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+
+class BookIssued(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
