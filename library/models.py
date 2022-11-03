c135b320084078571174a32fd330fe96a5dc99b8
jw-coder84 <jmw_19@hotmail.com>
2022-11-03 14:59:28 +0000

    Added db models to models.py, ran migrate, added cloudinary apps to settings

--- library/models.py
+++ library/models.py
@@ -4,19 +4,46 @@ from cloudinary.models import CloudinaryField
 import uuid
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    synopsis = models.TextField(max_length=1000, help_text='Enter a brief \
-        description of the book')
-    isbn = models.PositiveBigIntegerField(max_length=13, unique=True)
-    genre = models.CharField(max_length=50)
+    synopsis = models.TextField(
+        max_length=1000,
+        help_text='Enter a brief description of the book'
+    )
+    isbn = models.CharField('ISBN', max_length=13, unique=True)
+    genre = models.ManyToManyField(Genre)
 
     def __str__(self):
-        """String for representing the Model object."""
         return self.title
 
 
 class BookIssued(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
+    return_date = models.DateField(null=True, blank=True)
+    LOAN_STATUS = (
+        ('co', 'Checked out'),
+        ('a', 'Available'),
+    )
+
+    status = models.CharField(
+        max_length=2,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='a',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['return_date']
+
+    def __str__(self):
+        return f'{self.id} ({self.book.title})'
