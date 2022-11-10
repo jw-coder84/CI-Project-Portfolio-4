2ba7d1bbf29393849cad963b52e5388aa14082cb
jw-coder84 <jmw_19@hotmail.com>
2022-11-10 11:32:17 +0000

    Added html and css files, updated views.py

--- library/views.py
+++ library/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, Genre
 
-# Create your views here.
+
+class BookList(generic.ListView):
+    model = Book
+    queryset = Book.objects.order_by('-title')
+    template_name = 'index.html'
+    paginate_by = 6
