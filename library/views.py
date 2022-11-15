0bb6a6e7f21916f665536291fa36d2cb3e37de04
jw-coder84 <jmw_19@hotmail.com>
2022-11-15 15:32:54 +0000

    Added book_detail.html, referenced in urls, views.py and index.html

--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Book, Genre
 
 
@@ -8,3 +9,33 @@ class BookList(generic.ListView):
     queryset = Book.objects.order_by('title')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class BookDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Book.objects
+        post = get_object_or_404(queryset, slug=slug)
+        cover = Book.cover
+
+        return render(
+            request,
+            'book_detail.html',
+            {
+                'book': post,
+                'cover': cover,
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Book.objects
+        post = get_object_or_404(queryset, slug=slug)
+        cover = Book.cover
+
+        return render(
+            request,
+            'book_detail.html',
+            {
+                'book': post,
+                'cover': cover,
+            },
+        )
