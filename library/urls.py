4a9b5e9bad8331c440e3a98cb8d78c6c6779d0be
jw-coder84 <jmw_19@hotmail.com>
2022-11-16 10:15:45 +0000

    Updated library urls.py ref MEDIA_URL and MEDIA_ROOT in settings file to get pictures to display on site

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.BookList.as_view(), name="home"),
     path('<slug:slug>/', views.BookDetail.as_view(), name='book_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
