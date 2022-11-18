acacb6225a20db7c477c38e21a97bc49dbcbe7e8
jw-coder84 <jmw_19@hotmail.com>
2022-11-18 10:42:31 +0000

    Adding picture for new record, updating db model cover field

--- library/models.py
+++ library/models.py
@@ -15,7 +15,6 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     cover = models.ImageField(
-                    upload_to='media/',
                     null=True,
     )
     author = models.CharField(max_length=200)
