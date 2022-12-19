c98ff204f8b8632ba154fb5e26421f468c4561ab
jw-coder84 <jmw_19@hotmail.com>
2022-12-19 10:48:27 +0000

    Added automated tests for models and views

--- library/test_models.py
+++ library/test_models.py
@@ -4,10 +4,6 @@ from .models import BookIssued, Book
 
 class TestModels(TestCase):
 
-    def book_string_method_returns_name(self):
+    def test_book_string_method_returns_name(self):
         book = Book.objects.create(title='test book')
         self.assertEqual(str(book), 'test book')
-
-    # def book_issued_status_defaults_to_co(self):
-    #     book = BookIssued.objects.create(book='test book')
-    #     self.assertEqual(status, 'co')
--- library/tests_views.py
+++ library/tests_views.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import BookIssued, Book
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_get_index_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_get_book_list_page(self):
+        response = self.client.get('/books')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'book_list.html')
